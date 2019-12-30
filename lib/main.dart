386c6af8f680a1fa22c94bf8b2a101fe7d6ff240
james-muriithi <muriithijames556@gmail.com>
2019-12-30 04:37:39 +0300

    finished calculator

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,54 @@ class _MyHomePageState extends State<MyHomePage> {
   String operand = '';
 
   void buttonPressed(String buttonText) {
+    if(buttonText == 'CLEAR'){
+      _output = '0';
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = '';
+    }else if (buttonText == '+' || buttonText == '*' || buttonText == '/' || buttonText == '-') {
+      num1 = double.parse(output);
 
+      operand = buttonText;
+
+      _output = "0";
+    }else if (buttonText == '.' ) {
+      if(_output.contains(".")){
+        print("Already conatains a decimals");
+        return;
+
+      } else {
+        _output = _output + buttonText;
+      }
+    }else if (buttonText == "="){
+
+      num2 = double.parse(output);
+
+      if(operand == "+"){
+        _output = (num1 + num2).toString();
+      }
+      if(operand == "-"){
+        _output = (num1 - num2).toString();
+      }
+      if(operand == "*"){
+        _output = (num1 * num2).toString();
+      }
+      if(operand == "/"){
+        _output = (num1 / num2).toString();
+      }
+
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+
+    } else {
+
+      _output = _output + buttonText;
+
+    }
+    setState(() {
+      output = double.parse(_output).toStringAsFixed(2);
+    });
   }
 
   Widget _buildButton(String buttonText) {
